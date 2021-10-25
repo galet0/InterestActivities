1f687ff82636a5866b17142664c499526fecddfb
galet0 <galeto.miteva@gmail.com>
2021-10-25 14:55:02 +0300

    Create MyFirst MVC Project

---
+++ 02. HelloWorld/MyFirstMVCProject/MyFirstMVCProject/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstMVCProject.Models;
+
+namespace MyFirstMVCProject.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            Car car = new Car()
+            {
+                Brand = "BMW",
+                Model = "X5"
+            };
+
+            return View(car);
+        }
+
+        public string Welcome(string name, int id)
+        {
+            return $"Welcome to my page, {name}!ID = {id}";
+        }
+    }
+}
---
+++ 02. HelloWorld/MyFirstMVCProject/MyFirstMVCProject/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstMVCProject.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
